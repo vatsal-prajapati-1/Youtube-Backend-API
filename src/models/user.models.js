7d47be7b760296ce6b4ee618cba916822284dae7
vatsal-prajapati-1 <prajapativatsal555@gmail.com>
2025-04-19 19:55:20 +0530

    docs(user.models.js): remove redundant comments for avatar and coverImage fields
    The comments indicating "cloudinary url" for the `avatar` and `coverImage` fields were redundant as the purpose of these fields is already clear from the context. Removing these comments improves code readability by reducing unnecessary clutter.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -26,11 +26,11 @@ const userSchema = new Schema(
       index: true,
     },
     avatar: {
-      type: String, // cloudinary url
+      type: String, 
       required: true,
     },
     coverImage: {
-      type: String, // cloudinary url
+      type: String, 
     },
     watchHistory: [
       {
