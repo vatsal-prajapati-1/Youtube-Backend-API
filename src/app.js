403524aac2139d1f147366e0ed0a30ee30e2d54f
vatsal-prajapati-1 <prajapativatsal555@gmail.com>
2025-04-20 20:58:26 +0530

    feat(healthcheck): add healthcheck route and controller
    Introduce a new healthcheck endpoint to monitor the system's uptime and MongoDB connection status. This helps in quickly identifying and diagnosing issues with the application.

--- src/app.js
+++ src/app.js
@@ -23,8 +23,10 @@ import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
@@ -32,10 +34,5 @@ app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
-
-// home page
-app.get("/", (req, res) => {
-  res.send("Welcome to the Youtube Backend API");
-});
 
 export default app;
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+    mongoStatus:
+      mongoose.connection.readyState === 1 ? "Connected" : "Disconnected",
+  };
+
+  console.log(process.uptime(), "process uptime ");
+
+  try {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, healthcheck, "Health check successful"));
+  } catch (error) {
+    if (!error) throw new ApiError(404, "Error in health check");
+    healthcheck.message = error;
+    return res
+      .status(503)
+      .json(new ApiResponse(503, healthcheck, "Health check failed"));
+  }
+});
+
+export { healthcheck };
